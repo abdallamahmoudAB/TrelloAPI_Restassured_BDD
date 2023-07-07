293cdcbcf420688dc56ab1738aaba38abe455bf9
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-07-07 05:39:18 +0200

    add test annotation

--- src/test/java/boards/DeleteBoard.java
+++ src/test/java/boards/DeleteBoard.java
@@ -1,6 +1,7 @@
 package boards;
 
 import org.testng.ITestContext;
+import org.testng.annotations.Test;
 import utils.ConfigLoader;
 import utils.ContextAttribute;
 
@@ -8,6 +9,7 @@ import static io.restassured.RestAssured.given;
 
 public class DeleteBoard {
 
+    @Test
     public void DeleteBoardByID(ITestContext context){
 
 //        String boardID = (String) context.getAttribute("board_ID");
