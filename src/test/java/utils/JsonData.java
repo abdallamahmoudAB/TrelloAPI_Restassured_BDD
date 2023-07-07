8e42a0068bfb3e1f8eda3fafeb2149f7a2b833e0
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-07-07 05:04:30 +0200

    create JsonData Class to read json file

--- src/test/java/utils/JsonData.java
+++ src/test/java/utils/JsonData.java
@@ -1,2 +1,21 @@
-package utils;public class JsonData {
+package utils;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class JsonData {
+
+    public static JSONObject payload() throws FileNotFoundException {
+
+
+        File file = new File("./src/test/resources/payload.json"); // open the file
+        FileReader fileReader = new FileReader(file); // read the file
+        JSONTokener jsonTokener = new JSONTokener(fileReader); // pass into jsonTokener
+        JSONObject data = new JSONObject(jsonTokener); // extract data in jsonObject format
+        return data;
+    }
 }
