db7d8bce7fe842ea01fd91b443b803dec5a2b0b7
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-07-07 04:58:09 +0200

    create a class to load and read properties

--- src/test/java/utils/ConfigLoader.java
+++ src/test/java/utils/ConfigLoader.java
@@ -1,2 +1,24 @@
-package utils;public class ConfigLoader {
+package utils;
+
+import java.util.ResourceBundle;
+
+public class ConfigLoader {
+
+    private static ResourceBundle getValue(){
+        // load properties file
+        ResourceBundle config = ResourceBundle.getBundle("config"); // name of properties file
+        return config;
+    }
+
+    public static String getBaseUrl(){
+        return getValue().getString("base_url");
+    }
+
+    public static String getKey(){
+        return getValue().getString("key");
+    }
+
+    public static String getToken(){
+        return getValue().getString("token");
+    }
 }
