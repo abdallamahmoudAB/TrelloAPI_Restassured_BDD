69c2a92dd2eeda7752392890f3da091017e1baf2
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-07-07 04:39:47 +0200

    create putBoard TestCase

---
+++ src/test/java/boards/UpdateBoard.java
@@ -0,0 +1,29 @@
+package boards;
+import static io.restassured.RestAssured.*;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+public class UpdateBoard {
+
+    @Test
+    public void updateBoardByID(ITestContext context){
+
+        String boardID = (String) context.getAttribute("board_ID");
+
+        String payload = "{\n" +
+                "    \"name\": \"UpdatedBoardName\"\n" +
+                "}";
+
+        given()
+                .pathParam("id",boardID)
+                .queryParam("key", "d6473f6dab60a52e3586d511845510c7")
+                .queryParam("token", "ATTA3f18be01c7abe8f9aeb273b1d0e5ffead5c100ef50a76fba02324f8bb92dd2bc9B7E1F59")
+                .body(payload)
+
+                .when()
+                .get("https://api.trello.com/1/boards/")
+
+                .then()
+                .statusCode(200);
+    }
+}
